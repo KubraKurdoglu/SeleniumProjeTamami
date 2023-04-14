56140cde2b45dfec5bdcb115fa26ad3f921f16c2
KubraKurdoglu <kubrakurdoglusimsek@gmail.com>
2023-04-14 23:27:30 +0200

    14 avril

---
+++ src/test/java/slayttakiOdevler/ChatGbt.java
@@ -0,0 +1,29 @@
+package slayttakiOdevler;
+
+import org.junit.Test;
+import utilities.TestBase;
+
+public class ChatGbt extends TestBase {
+    //Tarayıcı sürücüsünü başlat
+
+    @Test
+    public void test01() {
+        /*
+
+
+    // Amazon anasayfasını aç
+        driver.get("https://www.amazon.com.tr/")
+
+    //Arama kutusunu bul ve bir kelime gir
+    search_box =driver.findElement("twotabsearchtextbox")
+            search_box.send_keys("laptop")
+    //Arama düğmesine tıkla
+    search_button =driver.find_element_by_xpath("//input[@value='Git']")
+            search_button.click()
+
+            //Tarayıcıyı kapat
+            driver.quit()
+
+         */
+}
+}
